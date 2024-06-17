4432f407c6943fbcd619bfa271b922fd72a97ed4
danibystrom <124156950+danibystrom@users.noreply.github.com>
2024-06-17 20:46:02 +0200

    should add new todo-spec (green)

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -4,8 +4,14 @@ describe("Todo Application", () => {
     cy.visit("/");
   });
 
-  it("passes", () => {
+  it("should load the homepage and find header text", () => {
     cy.visit("/");
     cy.get("h1").contains("On today's do's").should("be.visible");
+  });
+
+  it("should add a new todo", () => {
+    cy.get("input").type("New todo");
+    cy.get("button").click();
+    cy.get("li").contains("New todo").should("be.visible");
   });
 });
